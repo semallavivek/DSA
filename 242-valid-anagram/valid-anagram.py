bb8f1a68ee2cf009a85269724a4f1aa1aa82e775
semallavivek <semallavivek1@gmail.com>
2025-08-04 12:36:08 +0530

    Time: 1 ms (99.00%) | Memory: 18 MB (49.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,6 +2,9 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
      
         s_dici = Counter(s)
         t_dici = Counter(t)
